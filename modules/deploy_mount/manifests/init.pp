4d065b5fa2d8cf5af6c4bf9eaf77f5a3d94cf684
Luiz Philippe De Liso - Mac <philippeliso@gmail.com>
2019-09-20 22:40:04 -0300

    ajustes nfs

--- modules/deploy_mount/manifests/init.pp
+++ modules/deploy_mount/manifests/init.pp
@@ -17,7 +17,7 @@ class deploy_mount {
 	    options                 => 'lookupcache=pos,noatime,_netdev,rw,bg,soft,nfsvers=3,tcp,intr,noacl,nocto,rsize=8192,wsize=8192',
 	    dump                    => '0',
 	    pass                    => '0',
-	    require                 => [ Package ['nfs-utils'], File['nfs-mount-point'] ]
+	    require                 => [ Package ['nfs-utils'], File['/export'] ]
 	}
 
 }
