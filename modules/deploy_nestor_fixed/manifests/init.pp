eb712c0a72acea2c32f070df0e5d67195d28b467
Luiz Philippe De Liso - Mac <philippeliso@gmail.com>
2019-09-21 21:47:39 -0300

    incluindo configuracao correta do nestor

---
+++ modules/deploy_nestor_fixed/manifests/init.pp
@@ -0,0 +1,48 @@
+class deploy_nestor_fixed {
+
+	package { 'httpd':
+		ensure => 'present',
+	}
+
+	service { 'httpd':
+		enable      => true,
+		ensure      => running,
+		require		=> Package["httpd"]
+	}
+
+	file { '/etc/httpd/conf.d/userdir.conf':
+		ensure => absent,
+		require => Package['httpd'],
+		notify  => Service['httpd']
+	}
+
+	file { '/etc/httpd/conf.d/welcome':
+		ensure => absent,
+		require => Package['httpd'],
+		notify  => Service['httpd']
+	}
+
+	file { '/etc/httpd/conf.d/autoindex.conf':
+		ensure => absent,
+		require => Package['httpd'],
+		notify  => Service['httpd']
+	}
+
+	file { '/etc/httpd/conf/httpd.conf':
+		source   => "puppet:///modules/deploy_nestor_fixed/etc/httpd/conf/httpd.conf",
+		owner => "root",
+		group => "root",
+		mode => '0644',
+		require => Package['httpd'],
+		notify  => Service['httpd']
+	}
+
+	file { '/etc/httpd/conf.d/nestor.conf':
+		source   => "puppet:///modules/deploy_nestor_fixed/etc/httpd/conf.d/nestor.conf",
+		owner => "root",
+		group => "root",
+		mode => '0644',
+		require => Package['httpd'],
+		notify  => Service['httpd']
+	}
+}
