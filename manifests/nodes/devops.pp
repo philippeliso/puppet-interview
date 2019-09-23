9ccf04e6092010a549a0d933ab754e3fdb1997be
Luiz Philippe De Lliso - NI-14666-8 <lliso@uolinc.com>
2019-09-23 19:01:56 -0300

    fix

--- manifests/nodes/devops.pp
+++ manifests/nodes/devops.pp
@@ -1,4 +1,4 @@
-node 'br-sp1-a-devops-sv01.openstacklocal' {
+node 'br-sp1-a-devops-sv01.openstacklocal', 'br-sp1-a-devops-sv02.openstacklocal' {
     include deploy_hostnames, deploy_users#, deploy_mount
 }
 
