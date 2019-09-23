956b7438df9dc61b1884fc4189be72456589d2e7
Luiz Philippe De Lliso - NI-14666-8 <lliso@uolinc.com>
2019-09-23 19:22:59 -0300

    add devops2

--- modules/deploy_users/manifests/init.pp
+++ modules/deploy_users/manifests/init.pp
@@ -2,7 +2,7 @@ class deploy_users {
 
     case $hostname {
 
-        /^br-sp1-a-devops-sv01/: {
+        /^br-sp1-a-devops-sv0[1-2]/: {
 
            exec { 'ssh-keygen':
              path => "/usr/bin:/bin",
